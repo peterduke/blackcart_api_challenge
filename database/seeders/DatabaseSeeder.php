a329e39108e79afc6bf04b34b5c88d8ab7de0255
Peter Duke <peter.duke.10@gmail.com>
2020-11-02 11:41:06 -0500

    model, migration and DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,17 +2,28 @@
 
 namespace Database\Seeders;
 
+use App\Models\Store;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     * 
+     * Adds two records to the 'stores' table for Shopify and Woocommerce
      *
      * @return void
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $s1 = new Store;
+        $s1->id = 1;
+        $s1->platform = 'shopify';
+        $s1->save();
+
+        $s2 = new Store;
+        $s2->id = 2;
+        $s2->platform = 'woocommerce';
+        $s2->save();
     }
 }
